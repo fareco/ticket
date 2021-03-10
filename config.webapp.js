66ab96e7cf59f999380e11c563bac59a09a75ddb
sdjdd <i@sdjdd.com>
2021-03-10 17:18:55 +0800

    feat(config): custom metadata

--- config.webapp.js
+++ config.webapp.js
@@ -1,3 +1,5 @@
+/* eslint-disable react/display-name */
+import React from 'react'
 import { setConfig } from './modules/config'
 
 // Used in CustomerServiceStats.
@@ -5,3 +7,20 @@ import { setConfig } from './modules/config'
 setConfig('stats.offsetDays', -3)
 
 setConfig('weekendWarning.enabled', true)
+
+/* eslint-disable i18n/no-chinese-character */
+setConfig('ticket.metadata.customMetadata.comments', {
+  openId: '心动账号 ID',
+  userId: '角色 ID',
+  userName: '角色名',
+  deviceModel: '设备型号',
+  operatingSystem: '操作系统',
+})
+/* eslint-enable i18n/no-chinese-character */
+
+setConfig('ticket.metadata.customMetadata.valueRenderers', {
+  openId: (id) => <a href={`https://pt.xindong.com/kefu_issue/user_info/${id}`}>{id}</a>,
+  userId: (id) => <a href={`https://www.xdapp.com/smash/player/?sid=0&pid=${id}`}>{id}</a>,
+  ip: (ip) => <a href={`https://nali.leanapp.cn/ip/${ip}`}>{ip}</a>,
+  localDNS: (ip) => <a href={`https://nali.leanapp.cn/ip/${ip}`}>{ip}</a>,
+})
